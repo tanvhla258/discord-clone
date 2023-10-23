a0019419c11e0180edc0e02bf1c7bfe4e051aea7
tanvhla258 <tanvhla258@gmail.com>
2023-10-23 14:43:20 +0700

    server header

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,7 +1,31 @@
 import React from "react";
-
+import { initialProfile } from "@/lib/initial-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import { InitialModal } from "@/components/modals/initial-modal";
+import { ModeToggle } from "@/components/mode-toggle";
 const SetupPage = async () => {
-  return <div>SetupPage</div>;
+  const profile = await initialProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+  return (
+    <div>
+      <ModeToggle></ModeToggle>
+
+      <InitialModal></InitialModal>
+    </div>
+  );
 };
 
 export default SetupPage;
---
+++ components/ui/file-upload.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { UploadDropzone } from "@/lib/uploadthing";
+import { X } from "lucide-react";
+// import "@uploadthing/react/style.css";
+import Image from "next/image";
+interface FileUploadProps {
+  onChange: (url?: string) => void;
+  value: string;
+  endpoint: "messageFile" | "serverImage";
+}
+function FileUpload({ onChange, value, endpoint }: FileUploadProps) {
+  const fileType = value?.split(".").pop();
+  console.log(value);
+  if (value && fileType !== "pdf")
+    return (
+      <div className="relative h-20 w-20">
+        <Image src={value} alt={"upload"} fill className="rounded-full" />
+
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(error: Error) => {
+        console.log(error);
+      }}
+    ></UploadDropzone>
+  );
+}
+
+export default FileUpload;
